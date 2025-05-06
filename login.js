94ee4978d5f31666ceb60e3bc529c84b5c68d90c
kisanet shshay <shshaykisanet@gmail.com>
2025-05-06 21:14:31 +0300

    updated files

--- login.js
+++ login.js
@@ -1,29 +1,11 @@
-// export async function login(e) {
-//     e.preventDefault();
-//     const email = document.getElementById('email').value;
-//     const password = document.getElementById('password').value;
-  
-//     const res = await fetch('/api/login', {
-//       method: 'POST',
-//       headers: { 'Content-Type': 'application/json' },
-//       body: JSON.stringify({ email, password }),
-//     });
-  
-//     const data = await res.json();
-//     if (res.ok) {
-//       localStorage.setItem('token', data.token);
-//       window.location.href = 'index.html';
-//     } else {
-//       alert(data.message);
-//     }
-//   }
+
 
   async function login(e) {
     e.preventDefault();
     const email = document.getElementById('email').value;
     const password = document.getElementById('password').value;
   
-    const res = await fetch('/api/signup', {
+    const res = await fetch('/api/login', {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({ email, password })
@@ -32,7 +14,7 @@
     const data = await res.json();
     if (res.ok) {
       localStorage.setItem('token', data.token);
-      window.location.href = 'index.html';
+      window.location.href = 'login.html';
     } else {
       alert(data.message);
     }}
